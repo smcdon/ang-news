24f179445c6d9ab0961ad9cea04e1ded1f6ad583
Sean McDonald <ddmac14@gmail.com>
2014-04-20 02:42:26 +0000

    Initial commit; done everything short of adding voting to the application

---
+++ app/scripts/controllers/posts.js
@@ -0,0 +1,13 @@
+'use strict';
+
+app.controller('PostsCtrl', function ($scope, $location, Post) {
+  if($location.path() === '/') {
+    $scope.posts = Post.all;
+  }
+  $scope.post = {url: 'http://'};
+
+  $scope.deletePost = function (postId) {
+    Post.delete(postId);
+  };
+});
+
---
+++ app/scripts/controllers/postview.js
@@ -0,0 +1,13 @@
+'use strict';
+
+app.controller('PostViewCtrl', function ($scope, $routeParams, Post) {
+  $scope.post = Post.find($routeParams.postId);
+  
+  $scope.addComment = function () {
+    Post.addComment($routeParams.postId, $scope.comment);
+  };
+  
+  $scope.removeComment = function (comment, commentId) {
+    Post.deleteComment($scope.post, comment, commentId);
+  };
+});
